907aa26bb2f3a3b1d1ce186a2098792deff3f977
Ajayraj Singh <shiningcoders@gmail.com>
2021-11-01 10:36:52 +0530

    Add gesture detector for linking

--- lib/widgets/text_container.dart
+++ lib/widgets/text_container.dart
@@ -5,59 +5,66 @@ class TextContainer extends StatelessWidget {
   String? title;
   String? description;
   String? link;
+  Function? onTap;
   TextContainer({
     Key? key,
     @required this.title,
     @required this.description,
     this.link,
+    this.onTap,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 256.0,
-      width: 625.0,
-      padding: const EdgeInsets.all(
-        40.0,
-      ),
-      decoration: BoxDecoration(
-        color: context.theme.primaryColor,
-        borderRadius: BorderRadius.circular(12.0),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            '$title',
-            style: const TextStyle(
-              fontWeight: FontWeight.w600,
-              fontSize: 42.0,
-              color: Colors.white,
+    return GestureDetector(
+      onTap: () {
+        onTap;
+      },
+      child: Container(
+        height: 256.0,
+        width: 625.0,
+        padding: const EdgeInsets.all(
+          40.0,
+        ),
+        decoration: BoxDecoration(
+          color: context.theme.primaryColor,
+          borderRadius: BorderRadius.circular(12.0),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '$title',
+              style: const TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 42.0,
+                color: Colors.white,
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 10.0,
-          ),
-          Text(
-            '''$description''',
-            style: const TextStyle(
-              fontWeight: FontWeight.w600,
-              fontSize: 22.0,
-              color: Colors.white,
+            const SizedBox(
+              height: 10.0,
             ),
-          ),
-          link != null
-              ? Text(
-                  '$link',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 22.0,
-                    color: Colors.white,
-                  ),
-                )
-              : Container(),
-        ],
+            Text(
+              '''$description''',
+              style: const TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 22.0,
+                color: Colors.white,
+              ),
+            ),
+            link != null
+                ? Text(
+                    '$link',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 22.0,
+                      color: Colors.white,
+                    ),
+                  )
+                : Container(),
+          ],
+        ),
       ),
     );
   }
